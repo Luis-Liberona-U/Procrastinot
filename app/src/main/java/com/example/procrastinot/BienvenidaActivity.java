0ea37b85995f720e119e12aa9a39e19e3ae04e2d
Luis Liberona <luisliberonaurquieta@gmail.com>
2026-08-31 19:01:04 -0400

    Creación de vistas Bienvenida y Login

---
+++ app/src/main/java/com/example/procrastinot/BienvenidaActivity.java
@@ -0,0 +1,54 @@
+package com.example.procrastinot;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class BienvenidaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_bienvenida);
+
+        ViewCompat.setOnApplyWindowInsetsListener(
+                findViewById(R.id.main),
+                (v, insets) -> {
+
+                    Insets systemBars = insets.getInsets(
+                            WindowInsetsCompat.Type.systemBars()
+                    );
+
+                    v.setPadding(
+                            systemBars.left,
+                            systemBars.top,
+                            systemBars.right,
+                            systemBars.bottom
+                    );
+
+                    return insets;
+                }
+        );
+
+        Button btnIniciarSesion =
+                findViewById(R.id.btnIniciarSesion);
+
+        btnIniciarSesion.setOnClickListener(view -> {
+
+            Intent intent = new Intent(
+                    BienvenidaActivity.this,
+                    LoginActivity.class
+            );
+
+            startActivity(intent);
+        });
+    }
+}
